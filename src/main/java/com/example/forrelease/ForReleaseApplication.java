66041ca556bb3004638948033803961d2b49e8a7
kudongku <kudongku@naver.com>
2024-03-17 15:54:41 +0900

    chore: git init

---
+++ src/main/java/com/example/forrelease/ForReleaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.forrelease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForReleaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForReleaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/forrelease/ForReleaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forrelease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForReleaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
