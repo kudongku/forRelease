79f83f3686544bb53f4aa2162b4b5ea59490be3b
kudongku <kudongku@naver.com>
2024-03-29 02:40:29 +0900

    feat: mysql 없는 버전

--- src/main/java/com/example/forrelease/HiController.java
+++ src/main/java/com/example/forrelease/HiController.java
@@ -2,29 +2,23 @@ package com.example.forrelease;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class HiController {
 
-    private final HiService hiService;
-
-    @PatchMapping("/helloWorld/{hiStr}")
-    public void createHi(
-        @PathVariable String hiStr
-    ) {
-        hiService.createHi(hiStr);
-    }
-
     @GetMapping("/helloWorld/{hiStr}")
-    public List<Hi> getHi(
+    @ResponseBody
+    public String getHi(
         @PathVariable String hiStr
     ) {
-        return hiService.getHi(hiStr);
+        return "hello "+hiStr;
     }
 
 }
